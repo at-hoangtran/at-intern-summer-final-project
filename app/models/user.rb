aad2eb55afd6f0925952362b81f612c341f490ef
HoangTran <hoang.tran@asiantech.vn>
2018-08-08 07:45:44 +0700

    Basic Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,37 @@
 class User < ApplicationRecord
+  attr_accessor :activation_token
+
   before_save :downcase_email
+  before_create :create_activation_digest
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   PHONE_REGEX = /\A(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}\z/
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
   validates :name,  presence: true, length: { maximum: 50 }
-  # validates :phone, uniqueness: true, length: { maximum: 15 },
-  #                   format: { with: PHONE_REGEX }, numericality: true
+  validates :phone, uniqueness: true, length: { maximum: 15 },
+                    format: { with: PHONE_REGEX }, numericality: true
 
   has_secure_password
 
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   validates :password_confirmation, presence: true, length: { minimum: 6 }, allow_nil: true
 
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
   private
 
   def downcase_email
     self.email = email.downcase
   end
+
+  
 end
+
+
