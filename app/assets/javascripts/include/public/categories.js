9f77011de384b0a2eb02d9713577abca9594a891
Quan Nguyen H <quan.nguyen2@asiantech.vn>
2018-08-31 14:00:18 +0700

    test_2_4

--- app/assets/javascripts/include/public/categories.js
+++ app/assets/javascripts/include/public/categories.js
@@ -5,12 +5,15 @@ var categories = {
       category_id = head_category.dataset.categoryId
       var html = '';
       var template = $('#data-template').html();
+      var image_default = '/assets/no-product-image-0f35e2b34a82f17cac95766bab3727091fc29403eeb8c3241290ba8a086b600d.png';
       if (template) {
         $.each(data.obj, function (i, item) {
+          images = item.product_image.length
+          images = images > 0 ? item.product_image[0].url : image_default;
           if (item.product_category == category_id) {
             html += Mustache.render(template, {
               ID: item.id,
-              IMAGE: item.product_image[0].url,
+              IMAGE: images,
               PRICE: formatPrice(item.product_price),
               TIMER: fmtMSS(item.period)
             });
