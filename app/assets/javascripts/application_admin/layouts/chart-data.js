caf8ab777429a64b0cf49572ac13525c95e07edf
Quan Nguyen H <quan.nguyen2@asiantech.vn>
2018-08-31 10:06:23 +0700

    test_2_2

--- app/assets/javascripts/application_admin/layouts/chart-data.js
+++ app/assets/javascripts/application_admin/layouts/chart-data.js
@@ -7,13 +7,14 @@ var chart_data = {
 		chars = chart_data.request_order().reverse();
 		var lineChartData = {
 			labels: [
-				I18n.t("javascripts.application_admin.layouts.chart-data.monday"),
-				I18n.t("javascripts.application_admin.layouts.chart-data.tuesday"),
-				I18n.t("javascripts.application_admin.layouts.chart-data.wednesday"),
-				I18n.t("javascripts.application_admin.layouts.chart-data.thursday"),
-				I18n.t("javascripts.application_admin.layouts.chart-data.friday"),
-				I18n.t("javascripts.application_admin.layouts.chart-data.saturday"),
-				I18n.t("javascripts.application_admin.layouts.chart-data.sunday")
+				// I18n.t("javascripts.application_admin.layouts.chart-data.monday"),
+				// I18n.t("javascripts.application_admin.layouts.chart-data.tuesday"),
+				// I18n.t("javascripts.application_admin.layouts.chart-data.wednesday"),
+				// I18n.t("javascripts.application_admin.layouts.chart-data.thursday"),
+				// I18n.t("javascripts.application_admin.layouts.chart-data.friday"),
+				// I18n.t("javascripts.application_admin.layouts.chart-data.saturday"),
+				// I18n.t("javascripts.application_admin.layouts.chart-data.sunday")
+        'Thứ 2', 'Thứ 3', 'Thứ 4', 'Thứ 5', 'Thứ 6', 'Thứ 7', 'Chủ nhật'
 			],
 			datasets: [{
 				label: "My Second dataset",
